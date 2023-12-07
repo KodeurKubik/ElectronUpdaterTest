f626cf373674a511305322f4459886317f07b20d
Kubik <totorogaming.mc@gmail.com>
2023-12-07 18:39:59 +0100

    way too much stuff

--- index.js
+++ index.js
@@ -1,89 +1,28 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-const { autoUpdater } = require('electron-updater');
-const log = require('electron-log');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-console.log('App starting...');
-
-autoUpdater.autoDownload = true;
-autoUpdater.autoInstallOnAppQuit = true;
-
-app.whenReady().then(async () => {
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        fullscreenable: false,
-        frame: true,
-        maximizable: false,
-        resizable: true,
-        title: 'Some Electron Testing',
-        movable: true,
-        icon: "./assets/someicon.png",
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-
-    win.loadFile(`./page.html`, { hash: `v${app.getVersion()}` });
-
-    win.webContents.openDevTools();
-
-
-
-    win.once('ready-to-show', () => {
-        autoUpdater.checkForUpdates();
-    });
-
-
-    function sendStatusToWindow(text) {
-        log.info(text);
-        win.webContents.send('message', text);
-    }
-
-    autoUpdater.on('checking-for-update', () => {
-        sendStatusToWindow('Checking for update...');
-    })
-    autoUpdater.on('update-available', (info) => {
-        sendStatusToWindow('Update available.');
-    })
-    autoUpdater.on('update-not-available', (info) => {
-        sendStatusToWindow('Update not available.');
-    })
-    autoUpdater.on('error', (err) => {
-        sendStatusToWindow('Error in auto-updater. ' + err);
-    })
-    autoUpdater.on('download-progress', (progressObj) => {
-        let log_message = "Download speed: " + progressObj.bytesPerSecond;
-        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-        sendStatusToWindow(log_message);
-    })
-    autoUpdater.on('update-downloaded', (ev, info) => {
-        sendStatusToWindow('Update downloaded');
-
-        console.log(info);
-
-        let whatToDo = dialog.showMessageBoxSync(win, {
-            message: `An update is available! You're on version v${require('./package.json').version}. Newest is ${null}.`,
-            buttons: [
-                "OK",
-                "Install and Restart",
-            ],
-            defaultId: 0,
-            cancelId: 0,
-            detail: releaseNotes,
-            title: "New Update Available!",
+require('./updater')()
+    .then(async () => {
+        const win = new BrowserWindow({
+            width: 1280,
+            height: 720,
+            fullscreenable: false,
+            frame: true,
+            maximizable: false,
+            resizable: true,
+            title: 'Some Electron Testing',
+            movable: true,
             icon: "./assets/someicon.png",
-            type: 'question'
-        })
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false
+            }
+        });
 
-        // If install
-        if (whatToDo == 1) autoUpdater.quitAndInstall()
-    })
+        win.loadFile(`./page.html`, { hash: `v${app.getVersion()}` });
 
-    ipcMain.on('app_version', (event) => {
-        event.sender.send('app_version', { version: app.getVersion() });
+        // win.webContents.openDevTools();
+
+        ipcMain.on('app_version', (event) => {
+            event.sender.send('app_version', { version: app.getVersion() });
+        });
     });
-});
--- updater.js
+++ updater.js
@@ -1,18 +1,75 @@
+const { dialog, app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const { createWriteStream } = require('node:fs');
+
+let logger = createWriteStream('/Users/nini/Downloads/logg.txt', { flags: 'a' });
+
+const log = {
+    log: (data) => logger.write('\n' + JSON.stringify(data)),
+    info: (data) => logger.write('\n' + JSON.stringify(data)),
+    warn: (data) => logger.write('\n' + JSON.stringify(data)),
+    error: (data) => logger.write('\n' + JSON.stringify(data))
+}
 
 
+module.exports = () => {
+    return new Promise((resolve) => {
+        autoUpdater.logger = log;
+        log.log('App starting...');
+
+        autoUpdater.autoDownload = true;
+
+        app.whenReady().then(async () => {
+            const win = new BrowserWindow({
+                width: 500,
+                height: 528,
+                fullscreenable: false,
+                frame: true,
+                maximizable: false,
+                resizable: false,
+                title: 'Some Testing Updater',
+                movable: true,
+                icon: "./assets/someicon.png",
+                webPreferences: {
+                    nodeIntegration: true,
+                    contextIsolation: false
+                }
+            });
+
+            win.loadFile(`./updater.html`);
 
 
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-        type: 'info',
-        buttons: ['Restart', 'Later'],
-        title: 'Application Update',
-        message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail:
-            'A new version has been downloaded. Restart the application to apply the updates.'
-    }
+            win.once('ready-to-show', async () => {
+                win.webContents.send('statusUpdate', `Checking for updates...`);
+                let update = await autoUpdater.checkForUpdates();
+                if (update) {
+                    win.webContents.send('progressUpdate', 0);
+                    win.webContents.send('statusUpdate', `Update found! v${update.updateInfo.version}`);
+                }
+                else {
+                    win.webContents.send('statusUpdate', `Up to date! Opening the app!`);
+                    setTimeout(() => {
+                        win.close();
+                        return resolve(true);
+                    }, 1000)
+                }
+            });
 
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-        if (returnValue.response === 0) autoUpdater.quitAndInstall()
-    })
-})
+
+            autoUpdater.on('error', (err) => {
+                win.webContents.send('statusUpdate', `Something went wrong...`);
+                dialog.showErrorBox('An error occured...', `This shouldn't happend. Check your internet connection, restart the app and try again.\n\nFULL ERROR: ${err}`)
+            });
+
+            autoUpdater.on('download-progress', (progressObj) => {
+                win.webContents.send('progressUpdate', progressObj.percent);
+            });
+
+            autoUpdater.on('update-downloaded', () => {
+                win.webContents.send('statusUpdate', `Installing update...`);
+
+                autoUpdater.quitAndInstall();
+            })
+        });
+    });
+};
